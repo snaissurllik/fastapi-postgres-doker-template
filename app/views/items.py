5da61da130c775dbc03161e5625faba2bd595076
Arthur <artyrchir1@gmail.com>
2023-03-12 11:10:57 +0200

    app dir added

---
+++ app/views/items.py
@@ -0,0 +1,12 @@
+class Item:
+    def create(self, *args, **kwargs):
+        pass
+
+    def read(self, *args, **kwargs):
+        pass
+
+    def update(self, *args, **kwargs):
+        pass
+
+    def delete(self, *args, **kwargs):
+        pass
